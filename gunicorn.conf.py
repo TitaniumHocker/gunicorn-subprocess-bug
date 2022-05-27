dbe19e7369ee9683dfe13e6b71ee84be1e31c688
Ivan Fedorov <inbox@titaniumhocker.ru>
2022-05-27 03:27:55 +0300

    Added workaround

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,3 +4,13 @@ import subprocess
 def on_exit(server):
     for _ in range(10):
         subprocess.run(["/bin/sh", "./justexit.sh", "3"], capture_output=True)
+
+# Workaround
+# def on_exit(server):
+#     import threading
+#     def run_commands():
+#         for _ in range(10):
+#             subprocess.run(["/bin/sh", "./justexit.sh", "3"], capture_output=True)
+#     thread = threading.Thread(target=run_commands)
+#     thread.start()
+#     thread.join()
